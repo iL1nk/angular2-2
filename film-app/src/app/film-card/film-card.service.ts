761a56983655254d27830446fef4182a551fb0c3
iL1nk <ilink@ua.fm>
2017-05-29 23:16:24 +0300

    apikey added for film api

--- film-app/src/app/film-card/film-card.service.ts
+++ film-app/src/app/film-card/film-card.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable'
 @Injectable()
 export class FilmCardService {
   url: string = "http://www.omdbapi.com/?page=1&s="
+  apikey: string = "&apikey=520bbe17"
   constructor(private http: Http) { 
   }
 
@@ -14,7 +15,10 @@ export class FilmCardService {
   }
 
   getFilms(filmName: string) { // вызывать из компонента
-    return this.http.get(this.url + filmName).map(this.extractData)
+    return this.http.get(this.url + filmName + this.apikey).map(this.extractData)
   }
 
 }
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=520bbe17
+// Poster API: http://img.omdbapi.com/?i=tt3896198&h=600&apikey=520bbe17
